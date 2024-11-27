72e69cbcab5e55e027715859af2e74fcb40a21a4
TraiH <tramaine_hylton@hotmail.com>
2024-11-27 20:21:21 +0000

    object oriented exercise

--- exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/App.java
@@ -3,5 +3,9 @@ package com.cbfacademy.cars;
 public class App {
     public static void main(String[] args) {
         // TODO: Display details of all cars in showroom
+      
+
+
+        }
     }
-}
+
---
+++ exercises/object-oriented-programming/cars/src/main/java/com/cbfacademy/cars/Car.java
@@ -0,0 +1,39 @@
+package com.cbfacademy.cars;
+
+public class Car {
+
+private String make;
+private String model;
+private String colour;
+private int year;
+
+public Car(String make, String model, String colour, int year){
+
+this.make = make; 
+this.model = model;
+this.year = year;
+this.colour = colour;
+}
+
+public String getMake(){
+    return make;
+};
+public String getModel(){
+    return model;
+}
+
+public int getYear(){
+    return year;
+}
+public String getColour(){
+    return colour;
+};
+public void setColour(String colour){
+   this.colour = colour; 
+}
+public String getDetails(){
+  return make + ", " + model + ", " + year + ", " + colour;  
+};
+
+
+}
