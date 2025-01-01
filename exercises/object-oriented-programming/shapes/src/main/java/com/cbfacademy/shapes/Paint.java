593b4ba72c8a7db4202d2428916298c1ac65df99
TraiH <tramaine_hylton@hotmail.com>
2025-01-01 21:41:28 +0000

    paint exercise shape

---
+++ exercises/object-oriented-programming/shapes/src/main/java/com/cbfacademy/shapes/Paint.java
@@ -0,0 +1,14 @@
+package com.cbfacademy.shapes;
+
+public class Paint {
+
+    public Paint(double coverage){
+    }
+    
+    public getAmount(Shape shape){
+        if (amount <= area){
+            return amount;
+        }
+    }
+
+}
